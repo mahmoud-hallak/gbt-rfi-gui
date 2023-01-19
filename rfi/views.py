51a545b1a6c1f5fccebba5907a13f755b1ba24d8
Kathlyn Purcell <kpurcell@belinda.gb.nrao.edu>
2023-01-19 15:21:32 -0500

    Removing extra queries, creation of arrays, combining mulit_line with avg_plot and making all the plots use the reduced data not just the avg_line, finally re-organize for clarity and more concise functions

--- rfi/views.py
+++ rfi/views.py
@@ -6,6 +6,7 @@ import dateutil.parser as dp
 import matplotlib.dates as mdates
 import numpy as np
 import pandas as pd
+import plotly.express as px
 import plotly.graph_objs as go
 import plotly.offline as opy
 from plotly.subplots import make_subplots
@@ -13,14 +14,12 @@ from scipy.signal import find_peaks
 
 from django import forms
 from django.core.exceptions import ValidationError
-from django.db.models import FloatField
-from django.db.models.functions import Cast
 from django.shortcuts import render
 from django.utils.timezone import make_aware
 from django.views import View
 
 from .forms import QueryForm
-from .models import Frequency, Scan
+from .models import Frequency
 
 logger = logging.getLogger(__name__)
 
@@ -51,12 +50,11 @@ class DoGraph(View):
         self.request = request
         self.get_data()
         channels = self.filter_data()
-        div, data = self.create_avg_line(channels)
+        div, refined_data = self.create_avg_line(channels)
         if div == None:
             return self.plot_it(div)
         else:
-            div = self.create_color_plot(div, data)
-            div = self.create_multi_line(div, data)
+            div = self.create_color_plot(div, refined_data)
             return self.plot_it(div)
 
     def get_data(self):
@@ -96,13 +94,13 @@ class DoGraph(View):
             channels = channels.filter(frequency__gte=float(self.requested_freq_low))
         if self.requested_freq_high:
             channels = channels.filter(frequency__lte=float(self.requested_freq_high))
+
         # then we can either use the nearest date or a date range
-        scans = Scan.objects.filter(frontend__name__in=self.requested_receivers)
         if self.requested_date:
             # Get the nearest MJD (without scanning the whole table)
             try:
                 self.nearest_date = (
-                    scans.filter(datetime__lte=self.requested_date)
+                    channels.filter(scan__datetime__lte=self.requested_date)
                     .order_by("-datetime")
                     .first()
                     .datetime
@@ -118,7 +116,7 @@ class DoGraph(View):
                 channels = channels.filter(scan__datetime__lte=self.requested_end)
 
         else:
-            most_recent_scan = scans.latest("datetime")
+            most_recent_scan = channels.latest("scan__datetime")
             channels = channels.filter(scan=most_recent_scan)
 
 
@@ -127,18 +125,6 @@ class DoGraph(View):
         # channels = channels.annotate(mod=F("id") % 10).filter(mod=0)
         # channels = channels.order_by("scan__datetime", "channel")
         channels = channels.order_by("frequency")
-        '''
-        channel_aggregates = (
-            channels.values("scan__datetime")
-            .distinct()
-            .aggregate(
-                last_scan_start=Max("scan__datetime"),
-                first_scan_start=Min("scan__datetime"),
-            )
-        )
-        self.start = channel_aggregates["first_scan_start"]
-        self.end = channel_aggregates["last_scan_start"]
-        '''
 
         return channels
 
@@ -164,82 +150,45 @@ class DoGraph(View):
         prominence_val = max(proms)
         print(f"this is the prom val: {prominence_val}")
 
-        data = channels.annotate(
-            freq_float=Cast("frequency", FloatField()),
-            intensity_float=Cast("intensity", FloatField()),
-        ).values_list("freq_float", "intensity_float")
+        # set up the data to be used
+        data = pd.DataFrame(
+            channels.values("frequency", "intensity", "scan__datetime", "scan__session__name")
+            )
 
-        freq_vs_intensity = np.array(data)
-
-        if freq_vs_intensity.any():
-            #freq_vs_intensity = np.array(data)
+        if not data.empty:
             # Find local maxima (peaks) in intensities (second axis). This seems to reduce
             # the data by roughly a third, but doesn't throw away any spikes
             # prominence can reduce the data even further while preserving the peaks
-            intensity_peaks = find_peaks(freq_vs_intensity[:,1])[0]#, prominence=prominence_val)[0]
-            all_local_maximum_intensities = freq_vs_intensity[intensity_peaks]
+            intensity_peaks = find_peaks(data.intensity)[0]#, prominence=prominence_val)[0]
+            #print(f"{data.intensity.head(10)} {intensity_peaks[:10]}")
+            all_local_maximum_intensities = data[data.index.isin(intensity_peaks)]
             # NOTE: Can toggle this to make sure the shape stays the same
-            # to_plot = freq_vs_intensity
-            MAX_POINTS_TO_PLOT = 500_000
-            to_plot = all_local_maximum_intensities
-            if len(to_plot) > MAX_POINTS_TO_PLOT:
-                self.cache_form._errors["receivers"] = forms.ValidationError(f"Too many points: {len(to_plot):,}. Must be <{MAX_POINTS_TO_PLOT:,}. Select smaller ranges.")
+            MAX_POINTS_TO_PLOT = 550_000
+            refined_data = all_local_maximum_intensities
+            refined_data = refined_data.sort_values(by=["scan__session__name", "frequency"])
+            if len(refined_data.index) > MAX_POINTS_TO_PLOT:
+                self.cache_form._errors["receivers"] = forms.ValidationError(f"Too many points: {len(refined_data.index):,}. Must be <{MAX_POINTS_TO_PLOT:,}. Select smaller ranges.")
                 div = None
                 self.error_data_str = 'Maximum Data Queried - Reduce date or freq. ranges'
-                return div, data
+                return div, refined_data
 
-            print(f"Actual # {len(to_plot)}")
+            print(f"Actual # {len(refined_data.index)}")
 
-            # set up the data to be used
-            data = pd.DataFrame(
-                channels.values("frequency", "intensity", "scan__datetime", "scan__session__name")
-                )
-
-            data = data.sort_values(by=['scan__datetime'])
-            self.unique_days = data.scan__datetime.unique()
+            self.unique_days = refined_data.scan__datetime.unique()
 
             div=[]
+            avg_calculation = refined_data.groupby("frequency", as_index=False)['intensity'].mean()
+            avg_calculation = avg_calculation.assign(scan__session__name='Average')
 
-            # create the line plot and add it to div
-            graph = go.Scatter(
-                x=to_plot[:,0],
-                y=to_plot[:,1],
-                marker={"color": "black", "symbol": "circle", "size": 3},
-            )
-            start = data["scan__datetime"].min()
-            end = data["scan__datetime"].max()
-            if start == end:
-                self.date_range_str = start.date()
-            else:
-                self.date_range_str = (
-                    f"{start.date()} - "
-                    f"{end.date()}"
-                )
-            self.freq_range_str = f"{data.frequency.min():.2f}-{data.frequency.max():.2f}"
-            rcvr_names = {
-                "Prime Focus 1": "PF1",
-                "Rcvr_800": "PF1_800",
-                "Prime Focus 2": "PF2",
-                "Rcvr1_2": "L-Band",
-                "Rcvr2_3": "S-Band",
-                "Rcvr4_6": "C-Band",
-                "Rcvr8_10": "X-Band",
-                "Rcvr12_18": "Ku-Band",
-                "RcvrArray18_26": "KFPA",
-                "Rcvr26_40": "Ka-Band",
-                "Rcvr40_52": "Q-Band",
-                }
-            requested_receivers_name=[rcvr_names[i] for i in self.requested_receivers]
-            self.rcvr_str = ", ".join(requested_receivers_name)
-            title_line = "Averaged RFI Environment at Green Bank Observatory <br> <i>%s    %s    %s MHz</i>" % (self.date_range_str, self.rcvr_str, self.freq_range_str)
-            layout = go.Layout(
-                title=title_line,
-                title_x=0.5,
-                xaxis={"title": "Frequency (MHz)"},
-                yaxis={"title": "Intensity (Jy)"},
-            )
-            figure = go.Figure(data=graph, layout=layout)
-            div.append(opy.plot(figure, auto_open=False, output_type="div"))
+
+            layout = self.make_the_layout(refined_data)
+            # make the session line
+            fig_session = px.line(refined_data, x="frequency", y="intensity", color="scan__session__name", labels={"scan__session__name":"Session"})
+            fig_avg = px.line(avg_calculation, x="frequency", y="intensity", color="scan__session__name", labels={"scan__session__name":"Session"})
+            fig_avg.update_traces(line_color="black")
+
+            figure = go.Figure(data=fig_avg.data + fig_session.data, layout=layout)
+            div.append(opy.plot(figure, output_type='div'))
 
         else:
             # set div none, then skip color plot
@@ -247,9 +196,9 @@ class DoGraph(View):
             self.error_data_str = 'No Data - Check date or freq. ranges'
             self.plot_it(div)
 
-        return div, data
+        return div, refined_data
 
-    def create_color_plot(self, div, data):
+    def create_color_plot(self, div, refined_data):
         # make the color plot/s and add them to div
         session = 1
         [str(i.date()) for i in self.unique_days]
@@ -258,8 +207,8 @@ class DoGraph(View):
         for self.unique_day in self.unique_days:
             date_of_interest_datetime = self.unique_day.to_pydatetime()
 
-            unique_date_range = data[
-                data["scan__datetime"] == date_of_interest_datetime
+            unique_date_range = refined_data[
+                refined_data["scan__datetime"] == date_of_interest_datetime
             ]  # get the data but only for one session of interest at a time
             date_of_interest_datetime = date_of_interest_datetime.replace(tzinfo=None)
             # make the date bins for plotting
@@ -300,7 +249,7 @@ class DoGraph(View):
                 y=[str(self.unique_day.date())],
                 z=to_plot,
                 colorscale='Viridis',
-                coloraxis="coloraxis"
+                coloraxis="coloraxis", connectgaps = True,
                 ), row=session, col=1,
                 )
 
@@ -316,8 +265,44 @@ class DoGraph(View):
             fig.update_yaxes(tickformat="%b %d %Y", dtick=86400000)
 
         div.append(opy.plot(fig, auto_open=False, output_type="div"))
-
         return div
+
+    def make_the_layout(self, refined_data):
+        # make the layout
+        start = refined_data["scan__datetime"].min()
+        end = refined_data["scan__datetime"].max()
+        if start == end:
+            self.date_range_str = start.date()
+        else:
+            self.date_range_str = (
+                f"{start.date()} - "
+                f"{end.date()}"
+            )
+        self.freq_range_str = f'{refined_data["frequency"].min():.2f}-{refined_data["frequency"].max():.2f}'
+        rcvr_names = {
+            "Prime Focus 1": "PF1",
+            "Rcvr_800": "PF1_800",
+            "Prime Focus 2": "PF2",
+            "Rcvr1_2": "L-Band",
+            "Rcvr2_3": "S-Band",
+            "Rcvr4_6": "C-Band",
+            "Rcvr8_10": "X-Band",
+            "Rcvr12_18": "Ku-Band",
+            "RcvrArray18_26": "KFPA",
+            "Rcvr26_40": "Ka-Band",
+            "Rcvr40_52": "Q-Band",
+            }
+        requested_receivers_name=[rcvr_names[i] for i in self.requested_receivers]
+        self.rcvr_str = ", ".join(requested_receivers_name)
+        title_line = "RFI Environment at Green Bank Observatory <br> <i>%s    %s    %s MHz</i>" % (self.date_range_str, self.rcvr_str, self.freq_range_str)
+        layout = go.Layout(
+            title=title_line,
+            title_x=0.5,
+            xaxis={"title": "Frequency (MHz)"},
+            yaxis={"title": "Intensity (Jy)"},
+        )
+
+        return layout
 
     def plot_it(self, div):
         if div:
@@ -331,16 +316,3 @@ class DoGraph(View):
                 self.cache_form._errors["freq_low"] = forms.ValidationError(self.error_data_str)
                 self.cache_form._errors["freq_high"] = forms.ValidationError(self.error_data_str)
             return render(self.request, "rfi/query.html", {"form": self.cache_form})
-
-    def create_multi_line(self, div, data):
-        import plotly.express as px
-        fig = px.line(data.sort_values(by=['frequency']), x="frequency", y="intensity", color="scan__session__name", labels={"scan__session__name":"Session"})
-        fig.update_layout(
-            hovermode='x unified',
-            #legend_title="Session",
-            title={"text":f"RFI Environment at Green Bank Observatory per Session <br> <i>{self.date_range_str}    {self.rcvr_str}    {self.freq_range_str} MHz</i>", "x": 0.5},
-            xaxis_title="Frequency (MHz)",
-            yaxis_title="Intensity (Jy)",
-        )
-        div.append(opy.plot(fig, output_type='div'))
-        return div
